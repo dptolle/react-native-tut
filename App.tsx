5f7ef74b915cf83c80e129200842586fb06e558d
Dominic Tölle <dominic.tolle@gmail.com>
2020-10-21 16:06:05 +0200

    Add input handlers

--- App.tsx
+++ App.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
-import { View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, TextInput, StyleSheet } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={{ padding: 50}}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal"
-        style={{ width: '80%', padding: 10, borderColor: 'black', borderWidth: 1}}/>
-        <Button title="ADD" onPress={ () => {} }/>
-      </View>
-      <View>
+  const [enteredGoal, setEnteredGoal] = useState('');
 
+  const goalInputHandler = (enteredText: string) => setEnteredGoal(enteredText);
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
+
+  return (
+    <View style={style.screen}>
+      <View style={style.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={style.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
+      <View></View>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
